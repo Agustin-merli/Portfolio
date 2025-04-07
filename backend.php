887790ec59a65b62e673ee556d5e9d9b27c62ffc
Agustin-merli <agustinmerli81@gmail.com>
2025-04-07 19:41:39 -0300

    Primer commit

---
+++ backend.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/detalle.css">
+    <title>Ver Backend</title>
+</head>
+<body>
+
+    <div class="barra-superior">
+        <a href="detalle.php" class="volver">⬅️ Volver al proyecto</a>
+    </div>
+
+    <div class="contenedor">
+        <h1>🔐 Panel de Administración</h1>
+        
+        <img src="img/panel-web.png">
+
+        <p>Este backend permite administrar el contenido del sitio: noticias, categorías, usuarios y acceder rápidamente a la web.</p>
+
+        <h3>Inicio de sesión:</h3>
+        <img src="img/login-web.png">
+
+        <p>Solo usuarios registrados pueden ingresar. Se valida el email y contraseña.</p>
+
+        <h3>Listado de noticias:</h3>
+        <img src="img/listadoNoticias-web.png">
+
+        <p>Desde acá se pueden <strong>ver, crear, editar o eliminar noticias</strong>. También se muestran las categorías.</p>
+
+        <h3>Formulario de edición:</h3>
+        <img src="img/formEdit-web.png" class="edit-form">
+        <img src="img/formEdit2-web.png" class="edit-form">
+
+        <p>Permite modificar el título, descripción, texto, imagen, fecha, usuario, y categoría. Incluye validaciones.</p>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <title>Agustín Merli - Portafolio</title>
+</head>
+<body>
+
+    <div class="contenedor-datos">
+        <h1>Agustín Merli</h1>
+        <img src="img/imagen-perfil.png" class="foto-perfil">
+        <h2>Sobre mí</h2>
+        <p>Tengo 18 años y estoy estudiando Programación. Experiencia en HTML, PHP, JS y CSS.</p>
+
+        <div class="proyecto">
+            <h2>Proyectos</h2>
+            <h3>Mundo Informativo ⬇️</h3>
+            <img src="img/pagina-web.png">
+            <p>Un sitio web para publicar y administrar noticias con frontend y backend.</p>
+            <a href="detalle.php">Ver más</a>
+        </div>
+
+        <div class="contacto">
+            <h2>Contactos</h2>
+            <p>📧 agustinmerli81@gmail.com</p>
+            <p>📞 +54 9 2352-529014</p>
+        </div>
+    </div>
+</body>
+</html>
